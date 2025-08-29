b65253c0baee51bbd2e297b6349968461025a3ea
Ким | فيجا <105891882+TOPVEGA@users.noreply.github.com>
2025-08-29 05:30:47 +0300

    Add config.py from (HACKSESSION).zip

---
+++ config.py
@@ -0,0 +1,16 @@
+
+from asBASE import asJSON
+db = asJSON("as.json")
+
+SUDORS = [7829718443]
+
+
+
+class Config:
+    API_ID = 8186557
+    API_HASH = "efd77b34c69c164ce158037ff5a0d117"
+    TOKEN = "8005172009:AAGfhkYwejfs2uHzfKc8wBuWq7nDK7eK4jI"  
+    START_PIC = "https://graph.org/file/864483e9fb1cec38b67fe.jpg" 
+    CHAT = "VeGaOne" 
+   
+    bot_id = TOKEN.split(":")[0]    
